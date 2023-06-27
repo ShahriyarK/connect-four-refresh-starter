02f526896ba9e4a40aedb022221f82c59251bea6
Shahriyar Kashif <shahriyar.k@hotmail.com>
2023-06-27 16:22:58 +0500

    Connect four game final

--- starter/class/connect-four.js
+++ starter/class/connect-four.js
@@ -21,19 +21,130 @@ class ConnectFour {
     Screen.setGridlines(true);
 
     // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', ConnectFour.testCommand);
+    // Screen.addCommand('down', 'Moves the cursor down', this.cursor.down.bind(this.cursor));
+    // Screen.addCommand('up', 'Moves the cursor down', this.cursor.down.bind(this.cursor));
+    Screen.addCommand('left', 'Moves the cursor down', this.cursor.left.bind(this.cursor));
+    Screen.addCommand('right', 'Moves the cursor down', this.cursor.right.bind(this.cursor));
+    Screen.addCommand('x', 'Places X in the column', this.placeMove.bind(this, 'X'));
+    Screen.addCommand('o', 'Places O in the column', this.placeMove.bind(this, 'O'));
 
     this.cursor.setBackgroundColor();
     Screen.render();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
+  placeMove(char) {
+    const grid = Screen.grid;
+    const currentCol = this.cursor.col;
+    if (char === 'X') Screen.setMessage(`It is O's turn now.`)
+    else Screen.setMessage(`It is X's turn now.`)
+    for (let row = grid.length - 1; row >= 0; row--) {
+      if (grid[row][currentCol] === ' ') {
+        Screen.setGrid(row, currentCol, char);
+        const checkWinner = ConnectFour.checkWin(Screen.grid);
+        if (checkWinner) ConnectFour.endGame(checkWinner);
+        Screen.render();
+        return;
+      }
+    }
   }
 
   static checkWin(grid) {
+    //rotate matrix
+    const rotate = (grid) => {
+      let rotated = [];
+      const [width, height] = [grid[0].length, grid.length];
+      for (let col = 0; col < width; col++) {
+        let rowOfCols = []
+        for (let row = 0; row < height; row++) {
+          rowOfCols.push(grid[row][col])
+        }
+        rotated.push(rowOfCols)
+      }
+      return rotated;
+    }
+    const horizontalWin = getLinearWin(grid);
+    if (horizontalWin) return horizontalWin;
+    const rotatedGrid = rotate(grid);
+    const verticalWin = getLinearWin(rotatedGrid)
+    if (verticalWin) return verticalWin;
+    //Check horizontal
+    // for (let row = 0; row < grid.length; row++) {
+    //   let rowsOfFours = []
+    //   let startIdx = 0;
+    //   while (startIdx <= (grid[row].length - 4)) {
+    //     let endIndx = startIdx + 4;
+    //     let fourElRow = grid[row].slice(startIdx, endIndx);
+    //     if (fourElRow.length === 4) rowsOfFours.push(fourElRow);
+    //     startIdx++;
+    //   }
+    //   const winnerRow = rowsOfFours.find(row => row.every(el => el === row[0] && el !== ' '));
+    //   if (winnerRow) return winnerRow[0];
+    // }
+    function getLinearWin (grid) {
+      for (let row = 0; row < grid.length; row++) {
+        let rowsOfFours = []
+        let startIdx = 0;
+        while (startIdx <= (grid[row].length - 4)) {
+          let endIndx = startIdx + 4;
+          let fourElRow = grid[row].slice(startIdx, endIndx);
+          if (fourElRow.length === 4) rowsOfFours.push(fourElRow);
+          startIdx++;
+        }
+        const winnerRow = rowsOfFours.find(row => row.every(el => el === row[0] && el !== ' '));
+        if (winnerRow) return winnerRow[0];
+      }
+    }
+    //Check Vertical
+    // let rotatedGrid = rotate(grid);
+    // for (let row = 0; row < rotatedGrid.length; row++) {
+    //   let rowsOfFours = []
+    //   let startIdx = 0;
+    //   while (startIdx <= (rotatedGrid[row].length - 4)) {
+    //     let endIndx = startIdx + 4;
+    //     let fourElRow = rotatedGrid[row].slice(startIdx, endIndx);
+    //     if (fourElRow.length === 4) rowsOfFours.push(fourElRow);
+    //     startIdx++;
+    //   }
+    //   const winnerRow = rowsOfFours.find(row => row.every(el => el === row[0] && el !== ' '));
+    //   if (winnerRow) return winnerRow[0];
+    // }
+    // blank grid
+    if (grid.every(row => row.every(col => col === ' '))) return false;
 
+    //Check left diagonal
+    const [height, width] = [grid.length, grid[0].length]
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            let currentRow = row;
+            let currentCol = col;
+            let diagonals = [];
+            while (currentRow < height && currentCol < width) {
+                diagonals.push(grid[currentRow][currentCol])
+                currentRow++;
+                currentCol++;
+            }
+            if (diagonals.length === 4 && diagonals.every(el => el === diagonals[0] && el !== ' ')) return diagonals[0];
+        }
+    }
+
+    //Check right diagonals
+    for (let row = 0; row < height; row++) {
+      for (let col = width; col >- 0; col--) {
+          let currentRow = row;
+          let currentCol = col;
+          let diagonals = [];
+          while (currentRow < height && currentCol >= 0) {
+              diagonals.push(grid[currentRow][currentCol])
+              currentRow++;
+              currentCol--;
+          }
+          if (diagonals.length === 4 && diagonals.every(el => el === diagonals[0] && el !== ' ')) return diagonals[0];
+      }
+  }
+  //Check ties
+  if (grid.every(row => row.every(col => col !== ' '))) return 'T';
+
+  return false;
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
